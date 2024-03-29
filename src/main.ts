64d45c1d09ac97cbd9272370ff7e78a8a6f4c869
Ugochu1 <olinyaugochukwu1@gmail.com>
2024-03-29 14:23:26 +0100

    uninstall web3

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HTTPExceptionFilter } from './exceptions/filters/http.exception.filter';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const corsOptions: CorsOptions = {
+  origin: process.env.ALLOWED_CORS_ORIGINS.split(' '),
+  credentials: true, // always pass the header
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(corsOptions);
   app.useGlobalFilters(new HTTPExceptionFilter());
   await app.listen(8000);
 }
